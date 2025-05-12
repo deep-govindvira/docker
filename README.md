3b781da463f4b8d5134d2765afc0d2c3be3f3e68
Deep Govindvira <deepgovindvira@gmail.com>
2025-05-12 17:26:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# No need to download databases
+# No trouble for database configuration
+
+```
+git clone https://github.com/deep-govindvira/docker
+cd docker
+chmod +x run.sh
+./run.sh
+``` 
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start MongoDB container
+echo "Starting MongoDB container..."
+docker-compose -f mongo-db.yaml -p mongo-db up -d
+
+# Start MySQL container
+echo "Starting MySQL container..."
+docker-compose -f mysql-db.yaml -p mysql-db up -d
+
+# Start DynamoDB container
+echo "Starting DynamoDB container..."
+docker-compose -f dynamo-db-db.yaml -p dynamo-db up -d
+
+echo "All containers are up and running." 
